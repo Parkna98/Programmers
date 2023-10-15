6c38041d6ce69343fa9fa1817fbb58fb9167efce
Parkna98 <144654764+Parkna98@users.noreply.github.com>
2023-10-15 11:34:01 +0900

    [unrated] Title: 문자 개수 세기, Time: 0.03 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(String my_string) {
+        int[] answer = new int[52];
+        for(int i=0;i<my_string.length();i++){
+            char c=my_string.charAt(i);
+            if(c>='A'&&c<='Z')
+                answer[c-65]++;
+            else
+                answer[c-71]++;
+        }
+        return answer;
+    }
+}
