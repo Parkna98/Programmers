a9df81386cd9401434ae19e21d1e0433b5f154ba
Parkna98 <144654764+Parkna98@users.noreply.github.com>
2023-11-04 12:12:36 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.09 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,21 +1,22 @@
+import java.util.*;
 class Solution {
     public String solution(String s) {
-        StringBuilder answer = new StringBuilder();
-        boolean isToUpper=true;
-        
+        StringBuilder sb=new StringBuilder();
+        boolean isToUpper = true;
         for(char c:s.toCharArray()){
             if(!Character.isAlphabetic(c)){
-                answer.append(c);
                 isToUpper=true;
-            } else {
-                if(isToUpper){
-                    answer.append(Character.toUpperCase(c));
-                } else {
-                    answer.append(Character.toLowerCase(c));
+                sb.append(c);
+            }
+            else{
+                if(isToUpper)
+                    sb.append(Character.toUpperCase(c));
+                else{
+                    sb.append(Character.toLowerCase(c));
                 }
                 isToUpper=!isToUpper;
             }
         }
-        return answer.toString();
+        return sb.toString();
     }
 }
