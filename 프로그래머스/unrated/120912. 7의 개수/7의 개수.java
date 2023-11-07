760d00cf66badfcabdffa6796fc30a88448f6bec
Parkna98 <144654764+Parkna98@users.noreply.github.com>
2023-11-07 22:19:55 +0900

    [level unrated] Title: 7의 개수, Time: 14.09 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        String s="";
+        for(int i:array){
+            s+=i+"";
+        }
+        for(char c:s.toCharArray()){
+            if(c=='7') answer++;
+        }
+        return answer;
+    }
+}
